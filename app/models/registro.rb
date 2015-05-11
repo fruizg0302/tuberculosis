4bc1969f5a9f39456f641813f6f7faa2ab78d55b
fruizg0302 <fruizg0302@icloud.com>
2015-05-10 22:04:59 -0500

    Field added in model

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -1,7 +1,7 @@
 class Registro < ActiveRecord::Base
 
   belongs_to :state
-  attr_accessible :apellido_materno, :apellido_paterno, :email, :nationality_id, :nombre, :posgrado_id, :profession_id, :rfc, :workshop_id, :no_deposito_cuenta_bancaria,:state_id, :hospital_general, :service_id, :institution_id, :attendance, :user_id
+  attr_accessible :apellido_materno, :apellido_paterno, :email, :nationality_id, :nombre, :posgrado_id, :profession_id, :rfc, :workshop_id, :no_deposito_cuenta_bancaria,:state_id, :hospital_general, :service_id, :institution_id, :attendance, :user_id, :otro
 
   validates_presence_of :nombre, :apellido_paterno, :apellido_materno, :rfc, :email, :nationality_id, :profession_id, :no_deposito_cuenta_bancaria, :message => "No puede ir vacio"
 
